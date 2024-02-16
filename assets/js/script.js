7ba2dbefeda9531ccf4fdabcc33966fb4122cca0
davide laneri <davidelan@hotmail.com>
2024-02-16 08:29:09 +0000

    Add first main variables in javascrip

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let win; // 
+let illuminate;
+let count;
+let correct;
+let randomSequence = [];
+let playerSequence = [];
+let winRound = 3;
+let machineTurn;
+let flashInterval;
+let buzz = true;
