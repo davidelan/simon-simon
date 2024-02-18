eeb706489f62171f26184e5eb252e0b181230e8d
davide laneri <davidelan@hotmail.com>
2024-02-18 13:45:21 +0000

    Add comments to variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-let win; // 
-let illuminate;
-let count;
-let correct;
-let randomSequence = [];
-let playerSequence = [];
-let winRound = 3;
-let machineTurn;
-let flashInterval;
-let buzz = true;
+let win;            // when win is true the player has passed all rounds
+let illuminate;     // to assess flashing of colors 
+let count;          // keeps count of rounds     
+let correct;        // if true the player has clicked the right color
+let randomSequence = []; // array that contains random colors order
+let playerSequence = []; // array that keeps track of player clicked sequence
+let winRound = 3;   // number of rounds to win game
+let machineTurn;    // if true it's the computer turn
+let flashInterval;  // contains the the setinterval() for the gameturn() function
+let buzz = true;    // when true allow sounds associated with each color
+
