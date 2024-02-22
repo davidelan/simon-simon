b54b168264378a569acdf85392dfcde9ef7a10ae
davide laneri <davidelan@hotmail.com>
2024-02-22 12:09:23 +0000

    Change winRound to 5 rounds in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let count;          // keeps count of rounds
 let correct;        // if true the player has clicked the right color
 let randomSequence = []; // array that contains random colors order
 let playerSequence = []; // array that keeps track of player clicked sequence
-let winRound = 3;   // number of rounds to win game
+let winRound = 5;   // number of rounds to win game
 let machineTurn;    // if true it's the computer turn
 let flashInterval;  // contains the the setinterval() for the gameturn() function
 let buzz = true;    // when true allow sounds associated with each color
